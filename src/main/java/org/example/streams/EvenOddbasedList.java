4ec7b663ff6136eed10adf363ec00099a1f884ed
chandramoulisasubelli <cm.sasubelli@gmail.com>
2026-02-10 20:56:47 +0100

    java-basics

--- src/main/java/org/example/streams/EvenOddbasedList.java
+++ src/main/java/org/example/streams/EvenOddbasedList.java
@@ -12,11 +12,11 @@ public class EvenOddbasedList {
         numbers.stream().filter(number -> number % 2 == 0).forEach(System.out::println);
         numbers.stream().filter(number -> number % 2 != 0).forEach(System.out::println);
         Map<Boolean, List<Integer>> map = numbers.stream().mapToInt(Integer::intValue).boxed().collect(Collectors.partitioningBy(x -> x % 2 == 0));
-        map.entrySet().stream().forEach(System.out::println);
+        map.entrySet().forEach(System.out::println);
         List<Integer> evenbasedList = map.getOrDefault(true, List.of());
         List<Integer> oddbasedList2 = map.getOrDefault(false, List.of());
         evenbasedList.addAll(oddbasedList2);
         System.out.println(evenbasedList);
-        map.entrySet().stream().forEach(System.out::println);
+        map.entrySet().forEach(System.out::println);
     }
 }
