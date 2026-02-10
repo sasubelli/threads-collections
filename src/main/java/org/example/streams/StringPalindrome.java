4fa10b720b47ad04fb2a0c9f27fc932e44486236
chandramoulisasubelli <cm.sasubelli@gmail.com>
2026-02-10 20:55:20 +0100

    java-basics

--- src/main/java/org/example/streams/StringPalindrome.java
+++ src/main/java/org/example/streams/StringPalindrome.java
@@ -6,6 +6,6 @@ public class StringPalindrome {
     public static void main(String[] args) {
         List<String> input = List.of(new String[]{"abccba","xyyx", " ", "ewrwer","xyxsed"});
 
-        input.stream().filter(c -> new StringBuilder(c).reverse().toString().equals(c)).forEach(System.out::println);
+        input.stream().filter(c -> new StringBuilder(c).reverse().toString().equalsIgnoreCase(c)).forEach(System.out::println);
     }
 }
